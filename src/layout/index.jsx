4b2195f0bc7dbca764cafbe784416df85c542ea2
odiljonswe <karimovodilbek008@gmail.com>
2024-04-05 11:12:12 +0500

    update full

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -3,8 +3,14 @@ import { Outlet } from "react-router-dom";
 import App from "../App";
 
 const Layout = () => {
+  const token = localStorage.getItem("token");
   return (
     <>
+      {!token && (
+        <header>
+          <App />
+        </header>
+      )}
       <main className="w-full mx-auto max-w-[800px] mt-[50px]">
         <div>
           <Outlet />
